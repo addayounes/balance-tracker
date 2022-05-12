f0ca3abff594bfce4554c2ef35dc1de667dd9d7a
addy <addayounes8@gmail.com>
2022-05-12 19:59:47 +0100

    added record class

--- app/src/main/java/com/example/balancetracker/MainActivity.java
+++ app/src/main/java/com/example/balancetracker/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.balancetracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView balance, income, expense;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        balance = (TextView) findViewById(R.id.balance);
+        income = (TextView) findViewById(R.id.income);
+        expense = (TextView) findViewById(R.id.expense);
     }
 }
